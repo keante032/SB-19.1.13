a7ce8e2b9a481dce80996b57771b822402904e20
Kevin Templeton <templeton.kevin@gmail.com>
2022-07-21 14:05:01 -0500

    calc routes complete

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,37 @@
 # Put your app in here.
+from flask import Flask, request
+import operations 
+
+app = Flask(__name__)
+
+@app.route('/add')
+def add():
+    """Adds a and b and returns result as the body."""
+
+    a = requests.arg["a"]
+    b = requests.arg["b"]
+    return operations.add(a, b)
+
+@app.route('/sub')
+def sub():
+    """Subtracts b from a and returns result as the body."""
+
+    a = requests.arg["a"]
+    b = requests.arg["b"]
+    return operations.sub(a, b)
+
+@app.route('/mult')
+def mult():
+    """Multiplies a and b and returns result as the body."""
+
+    a = requests.arg["a"]
+    b = requests.arg["b"]
+    return operations.mult(a, b)
+
+@app.route('/div')
+def div():
+    """Divides a by b and returns result as the body."""
+
+    a = requests.arg["a"]
+    b = requests.arg["b"]
+    return operations.div(a, b)
